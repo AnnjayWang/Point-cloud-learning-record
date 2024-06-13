6c1a89f1af361b4c3f7ca20721fc31662fdd83f8
AnnjayWang <154001253+AnnjayWang@users.noreply.github.com>
2024-06-13 21:02:00 +0800

    Add files via upload

---
+++ 1.读取点云数据/open3d/read_pcd.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+'''读取txt文件,此函数只读取txt前三行'''
+def read_file(file_path):
+
+    """读取点云 np.array data"""
+    points = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            # 假设每行数据以逗号分隔，且数据顺序为x,y,z(只读取前三列坐标即可)
+            values = list(map(float, line.strip().split(',')))  # 逗号代表间隔是“,”，根据实际情况进行修改
+            points.append([values[0], values[1], values[2]])    # 这里只读取了前三列的数据，如果想读取后面的数据，自行添加values[3].....
+
+    np_data = np.array(points)
+    print("原始点云数量: %d"%np_data.shape[0])
+    #pc = o3d.geometry.PointCloud()  # 创建一个空的点云对象
+    #pc.points = o3d.utility.Vector3dVector(np_data)
+    #o3d.visualization.draw_geometries([pc], window_name = "原始点云")
+  
+    return np_data 
+
+
+
+
+def main():
+    
+    txt_filepath = 'E:\\深蓝学院三维点云课程\\modelnet40_normal_resampled\\airplane\\airplane_0001.txt'     # 所用txt为modelnet40, 六列数据分别为: x,y,z,nx,ny,nz。
+
+    np_data  = read_file(txt_filepath)
+
+if __name__ == '__main__':  
+    main()
